5307e08eb1e284a71c244b7d6c067c3a36a5e122
Mixamilian <2203mv@gmail.com>
2026-05-03 19:37:38 +0100

    Add files via upload
    Added second stage to each menu Option that asks for the scpecific table they would like data from

--- OOP library management system assessment/main.java
+++ OOP library management system assessment/main.java
@@ -25,23 +25,86 @@ public class main{
             System.out.println("Access Granted. Please choose 1 of the following.");
             System.out.println("1: Retrieve record");
             System.out.println("2: Update record");
-            System.out.println("3: Delete record");
-            System.out.println("4: Exit database");
+            System.out.println("3: Add new record");
+            System.out.println("4: Delete record");
+            System.out.println("5: Exit database");
             int choice = scanner.nextInt();
 
+            //Retrieval choice menu
             if (choice == 1){
+                System.out.println("From which table Would you like to retrieve from;");
+                System.out.println("1: book_management");
+                System.out.println("2: member_management");
+                System.out.println("3: borrowing_management");
+                int Rchoice1 = scanner.nextInt();
+                if (Rchoice1 == 1){
 
+                }
+                else if (Rchoice1 == 2){
+
+                }
+                else if (Rchoice1 == 3){
+
+                }
             }
 
+            //Update choice menu
             if (choice == 2){
+                System.out.println("Which table would ou like to update;");
+                System.out.println("1: book_management");
+                System.out.println("2: member_management");
+                System.out.println("3: borrowing_management");
+                 int Uchoice1 = scanner.nextInt();
+                if (Uchoice1 == 1){
 
+                }
+                else if (Uchoice1 == 2){
+
+                }
+                else if (Uchoice1 == 3){
+
+                }
             }
 
+
+            //new record Choice Menu
             if (choice == 3){
+                System.out.println("Into which table would you like to create a new record;");
+                System.out.println("1: book_management");
+                System.out.println("2: member_management");
+                System.out.println("3: borrowing_management");
+                 int Nchoice1 = scanner.nextInt();
+                if (Nchoice1 == 1){
 
+                }
+                else if (Nchoice1 == 2){
+
+                }
+                else if (Nchoice1 == 3){
+
+                }
             }
 
+            // Delete record choice menu
             if (choice == 4){
+                System.out.println("From which table Would you like to delete a record from;");
+                System.out.println("1: book_management");
+                System.out.println("2: member_management");
+                System.out.println("3: borrowing_management");
+                int Dchoice1 = scanner.nextInt();
+                 if (Dchoice1 == 1){
+
+                }
+                else if (Dchoice1 == 2){
+
+                }
+                else if (Dchoice1 == 3){
+
+                }
+            }
+
+            //exit System choice
+            if (choice == 5){
                 access = false;
             }
         }
